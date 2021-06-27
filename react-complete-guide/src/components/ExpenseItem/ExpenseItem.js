3c44d5d0377af435ae55884c6441fc79d7c74e6e
Vinicius Granado <vinicius.r.granado@gmail.com>
2021-06-26 22:32:15 -0300

    Update ExpenseItem component

--- react-complete-guide/src/components/ExpenseItem/ExpenseItem.js
+++ react-complete-guide/src/components/ExpenseItem/ExpenseItem.js
@@ -1,7 +1,13 @@
 const ExpenseItem = () => {
   return (
-    <h2>Expense Item</h2>
+    <div>
+      <div>June 26th 2021</div>
+      <div>
+        <h2>Car Insurance</h2>
+        <div>$294.67</div>
+      </div>
+    </div>
   );
-}
+};
 
 export default ExpenseItem;
