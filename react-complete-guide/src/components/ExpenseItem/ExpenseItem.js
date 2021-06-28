0f68a2324e2141564cf579fba88e27168b731956
Vinicius Granado <vinicius.r.granado@gmail.com>
2021-06-27 22:32:44 -0300

    Added dynamic data using variables

--- react-complete-guide/src/components/ExpenseItem/ExpenseItem.js
+++ react-complete-guide/src/components/ExpenseItem/ExpenseItem.js
@@ -1,12 +1,16 @@
 import './ExpenseItem.css';
 
 const ExpenseItem = () => {
+  const expenseDate = new Date(2021, 5, 27);
+  const expenseTitle = 'Car Insurance';
+  const expenseAmount = 294.67;
+
   return (
     <div className='expense-item'>
-      <div>June 26th 2021</div>
+      <div>{expenseDate.toISOString()}</div>
       <div className='expense-item__description'>
-        <h2>Car Insurance</h2>
-        <div className='expense-item__price'>$294.67</div>
+        <h2>{expenseTitle}</h2>
+        <div className='expense-item__price'>${expenseAmount}</div>
       </div>
     </div>
   );
