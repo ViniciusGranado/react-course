9b9f7a91f9cf46342321010d524a188d749952a5
Vinicius Granado <vinicius.r.granado@gmail.com>
2021-08-05 23:05:28 -0300

    Added ExpensesFilter implementation

--- react-complete-guide/src/components/NewExpense/NewExpense.js
+++ react-complete-guide/src/components/NewExpense/NewExpense.js
@@ -7,14 +7,14 @@ export const NewExpense = (props) => {
     const expenseData = {
       ...enteredExpenseData,
       id: Math.random().toString(),
-    }
+    };
 
     props.onAddExpense(expenseData);
-  }
+  };
 
   return (
     <div className="new-expense">
-      <ExpenseForm onSaveExpenseData={saveExpenseDataHandler}/>
+      <ExpenseForm onSaveExpenseData={saveExpenseDataHandler} />
     </div>
   );
 };
