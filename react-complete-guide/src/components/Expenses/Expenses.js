9336679c0983f3dfe2be4d2258cc5d27439bd02e
Vinicius Granado <vinicius.r.granado@gmail.com>
2021-08-10 23:37:26 -0300

    Added dinamically generated Expense items

--- react-complete-guide/src/components/Expenses/Expenses.js
+++ react-complete-guide/src/components/Expenses/Expenses.js
@@ -11,30 +11,23 @@ export const Expenses = (props) => {
     setFilteredYear(selectedYear);
   };
 
+  const getExpenseItems = (expenses) => {
+    return expenses.map((expense) => {
+      return (
+        <ExpenseItem
+          title={expense.title}
+          amount={expense.amount}
+          date={expense.date}
+      />
+      )
+    })
+  }
+
   return (
     <>
       <Card className="Expenses">
         <ExpensesFilter selected={filteredYear} onChangeFilter={filterChangeHandler} />
-        <ExpenseItem
-          title={props.items[0].title}
-          amount={props.items[0].amount}
-          date={props.items[0].date}
-        />
-        <ExpenseItem
-          title={props.items[1].title}
-          amount={props.items[1].amount}
-          date={props.items[1].date}
-        />
-        <ExpenseItem
-          title={props.items[2].title}
-          amount={props.items[2].amount}
-          date={props.items[2].date}
-        />
-        <ExpenseItem
-          title={props.items[3].title}
-          amount={props.items[3].amount}
-          date={props.items[3].date}
-        />
+        {getExpenseItems(props.items)}
       </Card>
     </>
   );
